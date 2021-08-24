8987ecc5e9f2e2091746b5d572e06a1224381649
math-sdo <matheus.oliveira@superlogica.com>
2021-08-23 22:33:06 -0300

    test: Adicionando primeiros controllers e routers

---
+++ code/super_gestao_app/app/Http/Controllers/SobreNosController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SobreNosController extends Controller
+{
+    public function sobreNos()
+    {
+        echo 'Então, eu sou o Math!';
+    }
+}
--- code/super_gestao_app/routes/web.php
+++ code/super_gestao_app/routes/web.php
@@ -10,17 +10,12 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|aaaaaaaa
+|
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Laravel 8.x
+// Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
 
-Route::get('/sobre-nos', function () {
-    return 'Sobre nós!';
-});
-
-Route::get('/contato', function () {
-    return 'Contato';
-});
+Route::get('/', 'PrincipalController@principal');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+Route::get('/contato', 'ContatoController@contato');
