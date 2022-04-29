de820d31294d78f5ab440afb8c4ca1f3049c15c0
Matheus Santos <matheussantos.it@outlook.com>
2022-04-29 18:27:30 -0300

    feat: Nomeando rotas

--- code/super_gestao_app/routes/web.php
+++ code/super_gestao_app/routes/web.php
@@ -13,19 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function() { return 'login'; })->name('site.login');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-
-Route::get('/contato', 'ContatoController@contato');
-
-Route::get('/login', function() { return 'login'; });
-
-Route::get('/clientes', function() { return 'clientes'; });
-
-Route::get('/fornecedores', function() { return 'fornecedores'; });
-
-Route::get('/produtos', function() { return 'produtos'; });
+Route::prefix('/app')->group(
+    function()
+    {
+        Route::get('/clientes', function() { return 'clientes'; })->name('app.clientes');
+        Route::get('/fornecedores', function() { return 'fornecedores'; })->name('app.fornecedores');
+        Route::get('/produtos', function() { return 'produtos'; })->name('app.produtos');
+    }
+);
 
 /* Teste com passagem de parãmetros
 
