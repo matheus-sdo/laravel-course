a81b6d16d5d4ce41f2648d4102dfcc1477bbe3ff
Matheus Santos <matheussantos.it@outlook.com>
2022-04-30 01:28:20 -0300

    test: Testando recursos da $loop

--- code/super_gestao_app/resources/views/app/fornecedor/index.blade.php
+++ code/super_gestao_app/resources/views/app/fornecedor/index.blade.php
@@ -6,10 +6,20 @@
 
 @isset($fornecedores)
     @forelse ($fornecedores as $key => $fornecedor)
+        <p>Iteração atual: {{ $loop->iteration }}</p>
         <p>Fornecedor: {{ $fornecedor['nome'] }}</p>
         <p>Status: {{ $fornecedor['status']}}</p>
         <p>CNPJ: {{ $fornecedor['cnpj'] ?? '' }}</p>
         <p>Telefone: {{ $fornecedor['ddd'] ?? '' }} {{ $fornecedor['telefone'] ?? ''}}</p>
+
+        @if ($loop->first)
+            <p>Primeiro iteração do loop</p>
+        @endif
+
+        @if ($loop->last)
+            <p>Último iteração do loop</p>
+            <p>Total de itens percorridos: {{ $loop->count }}</p>
+        @endif
         <hr/>
 
     @empty
