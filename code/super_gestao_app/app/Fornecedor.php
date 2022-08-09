60c954fc5d58349b822f1f7fc7d36b9041c7055f
Matheus Santos <matheussantos.it@outlook.com>
2022-08-08 23:04:13 -0300

    feat: Adicionando campos para serem preenchidos no método create

--- code/super_gestao_app/app/Fornecedor.php
+++ code/super_gestao_app/app/Fornecedor.php
@@ -8,4 +8,7 @@ class Fornecedor extends Model
 {
     // Ajustando nome da tabela
     protected $table = "fornecedores";
+
+    // Permitindo que estes parâmetros sejam recebidos pelo método create
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
