a295a2b4e56be695325f12eba3191261c45cd92b
Matheus Santos <matheussantos.it@outlook.com>
2022-08-16 00:27:22 -0300

    feat: Implementando SoftDelete

--- code/super_gestao_app/app/Fornecedor.php
+++ code/super_gestao_app/app/Fornecedor.php
@@ -3,12 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
+    // implementando soft delete
+    use SoftDeletes;
+
     // Ajustando nome da tabela
     protected $table = "fornecedores";
 
     // Permitindo que estes parâmetros sejam recebidos pelo método create
     protected $fillable = ['nome', 'site', 'uf', 'email'];
+
+
 }
