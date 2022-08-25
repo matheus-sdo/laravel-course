b8260a0c83543d45346ba993cc492f8c9f79abf9
Matheus Santos <matheussantos.it@outlook.com>
2022-08-25 00:05:56 -0300

    feat: Nova rota para validar e salvar os dados do campo

--- code/super_gestao_app/app/Http/Controllers/ContatoController.php
+++ code/super_gestao_app/app/Http/Controllers/ContatoController.php
@@ -9,20 +9,22 @@ class ContatoController extends Controller
 {
     public function contato(Request $request)
     {
-        $contato = new SiteContato();
+        return view('site.contato', ['titulo' => 'Contato (teste)']);
+    }
 
-        /*
-        $contato->nome = $request->input('nome');
-        $contato->telefone = $request->input('telefone');
-        $contato->email = $request->input('email');
-        $contato->motivo_contato = $request->input('motivo_contato');
-        $contato->mensagem = $request->input('mensagem');
+    public function salvar(Request $request)
+    {
+        // Validando se os campos do formulário estão preenchidos
+        $request->validate(
+            [
+                'nome'           => 'required',
+                'telefone'       => 'required',
+                'email'          => 'required',
+                'motivo_contato' => 'required',
+                'mensagem'       => 'required',
+            ],
+        );
 
-        $contato->save();
-        */
-
-        $contato->create($request->all());
-
-        return view('site.contato');
+        SiteContato::create($request->all());
     }
 }
