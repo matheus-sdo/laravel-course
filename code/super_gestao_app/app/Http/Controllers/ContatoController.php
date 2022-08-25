449ba148f6f27a5863cc6d2e262940f192c9d9b2
Matheus Santos <matheussantos.it@outlook.com>
2022-08-24 23:22:06 -0300

    feat: Utilizando fill para salvar direto do array

--- code/super_gestao_app/app/Http/Controllers/ContatoController.php
+++ code/super_gestao_app/app/Http/Controllers/ContatoController.php
@@ -11,12 +11,17 @@ class ContatoController extends Controller
     {
         $contato = new SiteContato();
 
+        /*
         $contato->nome = $request->input('nome');
         $contato->telefone = $request->input('telefone');
         $contato->email = $request->input('email');
         $contato->motivo_contato = $request->input('motivo_contato');
         $contato->mensagem = $request->input('mensagem');
 
+        $contato->save();
+        */
+
+        $contato->fill($request->all());
         $contato->save();
 
         return view('site.contato');
