24c93d0db9d6139e4ef35ac140940730fab6e009
Matheus Santos <matheussantos.it@outlook.com>
2022-08-24 23:18:52 -0300

    feat: Salvando objeto SiteContato

--- code/super_gestao_app/app/Http/Controllers/ContatoController.php
+++ code/super_gestao_app/app/Http/Controllers/ContatoController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $request)
     {
-        var_dump($_POST);
+        $contato = new SiteContato();
+
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+        $contato->save();
+
         return view('site.contato');
     }
 }
