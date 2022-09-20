a9a18c92a17d3e2bf86e035e5c078a0de6ff4307
Matheus Santos <matheussantos.it@outlook.com>
2022-09-20 20:28:30 -0300

    refactory: Exibindo erros da forma correta

--- code/super_gestao_app/resources/views/site/layouts/_components/form_contato.blade.php
+++ code/super_gestao_app/resources/views/site/layouts/_components/form_contato.blade.php
@@ -2,10 +2,15 @@
 <form action={{ route('site.contato') }} method="post">
     @csrf
     <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
+    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
     <br>
+
     <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
+    {{ $errors->has('telefone') ? $errors->first('telefone') : '' }}
     <br>
+
     <input name="email" value="{{ old('email') }}"type="text" placeholder="E-mail" class="{{ $classe }}">
+    {{ $errors->has('email') ? $errors->first('email') : '' }}
     <br>
 
     <select name="motivo_contatos_id" class="{{ $classe }}">
@@ -15,14 +20,12 @@
             <option value="{{$motivo->id}}" {{ old('motivo_contatos_id') == $motivo->id ? 'selected' : ''}}>{{$motivo->motivo_contato}}</option>
         @endforeach
     </select>
+    {{ $errors->has('motivo_contatos_id') ? $errors->first('motivo_contatos_id') : '' }}
     <br>
+
     <textarea name="mensagem" class="{{ $classe }}">{{ old('mensagem') ?: "Preencha aqui a sua mensagem"}}</textarea>
+    {{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}
     <br>
+
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
-
-<div style="position: absolute; top: 0px; width: 100%; left: 0px; background: #000; color: #fff">
-    <pre>
-        {{ print_r($errors) }}
-    </pre>
-</div>
