4fbc0e3216c769ae893832be2f41305865a495e3
Matheus Santos <matheussantos.it@outlook.com>
2022-09-20 21:03:05 -0300

    feat: Inserindo mensagens de erro personalizadas

--- code/super_gestao_app/app/Http/Controllers/ContatoController.php
+++ code/super_gestao_app/app/Http/Controllers/ContatoController.php
@@ -17,16 +17,26 @@ class ContatoController extends Controller
 
     public function salvar(Request $request)
     {
-        // Validando se os campos do formulário estão preenchidos
-        $request->validate(
-            [
-                'nome'               => 'required|min:3|max:50',
-                'telefone'           => 'required',
-                'email'              => 'email',
-                'motivo_contatos_id' => 'required',
-                'mensagem'           => 'required|max:2000',
-            ],
-        );
+        $regras = [
+            'nome'               => 'required|min:3|max:50|unique:site_contatos',
+            'telefone'           => 'required',
+            'email'              => 'email',
+            'motivo_contatos_id' => 'required',
+            'mensagem'           => 'required|max:2000',
+        ];
+
+        $feedback = [
+            'nome.min'                    => 'O campo "Nome" precisa ter no mínimo 3 caracteres.',
+            'nome.max'                    => 'O campo "Nome" precisa ter no máximo 40 caracteres.',
+            'nome.unique'                 => 'O nome informado já está em uso.',
+            'email.email'                 => 'O e-mail inserido não é valido.',
+            'motivo_contatos_id.required' => 'Selecione um motivo de contato.',
+            'required'                    => 'O campo ":attribute" é obrigatório.',
+            'mensagem.max'                => "A mensagem deve ter no máximo 2000 caracteres."
+        ];
+
+        // Validando se os campos do formulário estão preenchidos e retornando erros personalizados
+        $request->validate($regras, $feedback);
 
         SiteContato::create($request->all());
 
