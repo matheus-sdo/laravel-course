c784a340c6df43d11482e0a718fa8a17a91ac6a9
Matheus Santos <matheussantos.it@outlook.com>
2023-11-24 21:01:08 -0300

    Listando fornecedores conforme as informações buscadas

--- code/super_gestao_app/resources/views/app/fornecedor/listar.blade.php
+++ code/super_gestao_app/resources/views/app/fornecedor/listar.blade.php
@@ -16,8 +16,31 @@
         </div>
 
         <div class="informacao-pagina">
-            <div style="width: 30%; margin: 0 auto;">
-                Tô listando tudo veyr
+            <div style="width: 90%; margin: 0 auto;">
+                <table border='1' width='100%'>
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Site</th>
+                            <th>UF</th>
+                            <th>E-mail</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($fornecedores as $fornecedor)
+                            <tr>
+                                <td>{{$fornecedor->nome}}</td>
+                                <td>{{$fornecedor->site}}</td>
+                                <td>{{$fornecedor->uf}}</td>
+                                <td>{{$fornecedor->email}}</td>
+                                <td>Editar</td>
+                                <td>Excluir</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
