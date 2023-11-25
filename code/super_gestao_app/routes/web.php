66fb87f2d65fc72c1a9812c648090636ad530a79
Matheus Santos <matheussantos.it@outlook.com>
2023-11-24 22:35:41 -0300

    Implementando a exclusão de um fornecedor

--- code/super_gestao_app/routes/web.php
+++ code/super_gestao_app/routes/web.php
@@ -36,6 +36,7 @@ Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(
         Route::get('/fornecedor/adicionar', 'FornecedorController@adicionar')->name('app.fornecedor.adicionar');
         Route::post('/fornecedor/adicionar', 'FornecedorController@adicionar')->name('app.fornecedor.adicionar');
         Route::get('/fornecedor/editar/{id}/{msg?}', 'FornecedorController@editar')->name('app.fornecedor.editar');
+        Route::get('/fornecedor/excluir/{id}', 'FornecedorController@excluir')->name('app.fornecedor.excluir');
 
         Route::get('/produto', 'ProdutoController@index')->name('app.produto');
     }
