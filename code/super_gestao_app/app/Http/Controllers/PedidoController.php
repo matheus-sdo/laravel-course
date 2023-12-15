808d2379886f9253773c59926e10fd444133d170
Matheus Santos <matheussantos.it@outlook.com>
2023-12-15 00:10:51 -0300

    Registrando um pedido no DB

--- code/super_gestao_app/app/Http/Controllers/PedidoController.php
+++ code/super_gestao_app/app/Http/Controllers/PedidoController.php
@@ -8,6 +8,14 @@ use Illuminate\Http\Request;
 
 class PedidoController extends Controller
 {
+    protected $regras = [
+        'cliente_id' => 'exists:clientes,id'
+    ];
+
+    protected $feedback = [
+        'cliente_id.exists' => 'O cliente informado não existe.'
+    ];
+
     /**
      * Display a listing of the resource.
      *
@@ -38,7 +46,13 @@ class PedidoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->regras, $this->feedback);
+
+        $pedido = new Pedido();
+        $pedido->cliente_id = $request->get('cliente_id');
+        $pedido->save();
+
+        return redirect()->route('pedido.index');
     }
 
     /**
