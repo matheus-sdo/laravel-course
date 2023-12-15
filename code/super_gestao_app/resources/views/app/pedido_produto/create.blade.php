d58bfc7907ee0650805d9f9613689f8f48662af5
Matheus Santos <matheussantos.it@outlook.com>
2023-12-15 15:36:10 -0300

    Exibindo os produtos

--- code/super_gestao_app/resources/views/app/pedido_produto/create.blade.php
+++ code/super_gestao_app/resources/views/app/pedido_produto/create.blade.php
@@ -22,6 +22,23 @@
             <p>Cliente: {{$pedido->cliente_id}}</p>
 
             <div style="width: 30%; margin: 0 auto;">
+                <h4>Itens do pedido:</h4>
+                <table border='1' width='100%'>
+                    <thead>
+                        <tr>
+                            <td>ID</td>
+                            <td>Nome</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($pedido->produtos as $produto)
+                        <tr>
+                            <td>{{ $produto->id }}</td>
+                            <td>{{ $produto->nome }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
                 @component('app.pedido_produto._components.form_create', ['pedido' => $pedido, 'produtos' => $produtos])
                 @endcomponent
             </div>
